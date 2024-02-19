4c24d11bc08f902bc8c037f6707aa43422229e7a
shivam_since2002 <sg2109964@gmail.com>
2024-02-19 20:54:15 +0530

    Using 2 method to store data

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasee/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:firebasee/firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -20,17 +17,51 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
+    FirebaseFirestore firebase = FirebaseFirestore.instance;
+    //Add data
+    void addData() async {
+      await firebase.collection("List in Fire").add(
+            ({"Om": "Jai Shree Ram"}),
+          );
+    }
+
+    //Add Data 2
+    void addDataTwo() async {
+      //Create a new Document Reference
+      DocumentReference documentReference =
+          firebase.collection("users").doc("user 1");
+      //Set data on the document
+      await documentReference.set(
+        {'name': 'Shivam Gupta', 'age': 30, 'email': 'sg2109964@gmail.com'},
+      );
+    }
+
+    print("Sucessfully Created");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Center(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                "Click Here",
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    addData();
+                  },
+                  child: const Text(
+                    "Add Data 1",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    addDataTwo();
+                  },
+                  child: const Text(
+                    "Add Data 2",
+                  ),
+                ),
+              ],
             ),
           ),
         ),
